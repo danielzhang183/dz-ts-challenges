0b95379a362f7e54ef27cada263a92fb88e1854a
danielzhang183 <danielzhang182@gmail.com>
2023-04-06 06:48:20 +0800

    feat: init with vitest

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+useHead({
+  title: 'TS Challenge',
+  meta: [
+    { name: 'description', content: 'TS Challenge' },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+    },
+  ],
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
